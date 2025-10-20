1cb5ad7c8b2a8c0dd1e8e6c09671c2fa9effb3eb
Gamuchirai Yolanda Munyengwa <167666881+Yolagamu@users.noreply.github.com>
2025-10-20 19:18:59 +0200

    Update index.ts

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,1 +1,19 @@
+export interface PropertyProps {
+  name: string;
+  rating: number;
+  address: {
+    city: string;
+    country: string;
+  };
+  image: string;
+  description: string;
+  category: string[];
+  price: number;
+  reviews: {
+    name: string;
+    avatar: string;
+    rating: number;
+    comment: string;
+  }[];
+}
 
